68a03911c3c036c677f86ddb3f468605cda9717d
Ashvini Bhosale <ashvini.bhosale@weboniselab.com>
2015-01-22 10:55:38 +0530

    completed login  with showing and deleting users

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,70 @@
+module SessionsHelper
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def current_user
+   if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+     end
+end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user_id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+   end
+
+   def forget(user)
+    # user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+   end
+
+   def log_out
+     # forget(current_user)
+     session.delete(:user_id)
+     @current_user = nil
+   end
+
+   def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+   def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+end
